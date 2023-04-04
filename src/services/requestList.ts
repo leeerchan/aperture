e40d1897e5619a6c905e71c5e8a5fabe42de4bb9
leeerchan <67493492+leeerchan@users.noreply.github.com>
2023-04-04 21:57:21 +0800

    Update requestList.ts

--- src/services/requestList.ts
+++ src/services/requestList.ts
@@ -6,8 +6,8 @@ import cookie from 'react-cookies';
 
 export const getResource = () => axios.get(`/api/resource`);
 
-//const prefix = "http://192.168.43.133:8080/DataBase/V1/";
-const prefix = "http://110.42.214.206:9090/DataBase/V1/";
+const prefix = "http://192.168.43.133:9090/DataBase/V1/";
+//const prefix = "http://110.42.214.206:9090/DataBase/V1/";
 
 // 登陆注册接口
 export const registerUser = (email: String, pwd: String) => axios.post(`${prefix}registerUser`, 
